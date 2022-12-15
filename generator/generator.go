ca025a8dad6371e8f2438b5df2fe3bf99f1b3bb2
Mario Toffia <mario.toffia@gmail.com>
2022-12-15 10:38:31 +0100

    composition done

--- generator/generator.go
+++ generator/generator.go
@@ -98,7 +98,7 @@ func ProcessSpecification(ctx *GeneratorContext, schemas map[string]*openapi3.Sc
 				filepath.Base(ctx.settings.spec), componentName), ctx.settings.spec_root,
 		)
 
-		comp, err := CreateComponent(ctx, id, v)
+		comp, err := CreateComponentFromReference(ctx, id, v)
 		if err != nil {
 			return err
 		}
@@ -111,51 +111,138 @@ func ProcessSpecification(ctx *GeneratorContext, schemas map[string]*openapi3.Sc
 	return nil
 }
 
-// CreateComponent will create the `ComponentDefinition` and return it. If there are sub-components such
+// CreateComponentFromReference will create the `ComponentDefinition` and return it. If there are sub-components such
 // as references or `allOf`, `oneOf` or `anyOf` they will be created as well.
 //
 // The types and components will be registered in the resolver so no duplicates will be created.
-func CreateComponent(ctx *GeneratorContext, componentId *gentypes.ComponentReference, ref *openapi3.SchemaRef) (*gentypes.ComponentDefinition, error) {
+func CreateComponentFromReference(
+	ctx *GeneratorContext,
+	componentId *gentypes.ComponentReference,
+	ref *openapi3.SchemaRef) (*gentypes.ComponentDefinition, error) {
 	// Been here before?
 	if ctx.resolver.ResolveComponent(componentId) != nil {
 		return nil, nil
 	}
 
+	if ref.Ref == "" {
+		return CreateComponentFromDefinition(
+			ctx, componentId, ref.Value,
+		)
+	}
+	// This is a reference
 	component := &gentypes.ComponentDefinition{
 		ID:         *componentId,
-		Reference:  nil,
+		Reference:  ResolveReferenceAndSwitchIfNeeded(ctx, componentId, ref),
 		Definition: nil,
 	}
 
-	if ref.Ref == "" {
-		// This is a inline type
-		td := gentypes.TypeDefinition{
-			ID:          component.ID,
-			GoPackage:   ResolveGoPackage(ctx, &component.ID),
-			Schema:      ref.Value,
-			Composition: []gentypes.Composition{},
-			Properties:  []gentypes.Property{},
+	cached_type := ctx.resolver.ResolveComponent(component.Reference)
+
+	if cached_type != nil {
+		return component, nil
+	}
+
+	CreateComponentFromDefinition(ctx, component.Reference, ref.Value)
+	return component, nil
+}
+
+func CreateComponentFromDefinition(
+	ctx *GeneratorContext,
+	componentId *gentypes.ComponentReference,
+	def *openapi3.Schema) (*gentypes.ComponentDefinition, error) {
+	// Been here before?
+	if ctx.resolver.ResolveComponent(componentId) != nil {
+		return nil, nil
+	}
+
+	td := gentypes.TypeDefinition{
+		ID:          *componentId,
+		GoPackage:   ResolveGoPackage(ctx, componentId),
+		Schema:      def,
+		Composition: []gentypes.Composition{},
+		Properties:  []gentypes.Property{},
+	}
+
+	component := &gentypes.ComponentDefinition{
+		ID:         *componentId,
+		Reference:  nil,
+		Definition: &td,
+	}
+
+	// Register the component so it will be resolved if cyclic references
+	ctx.resolver.RegisterComponent(component)
+
+	// Composition
+	if len(def.AllOf) > 0 {
+
+		// Extract *all* inline objects first
+		var err error
+
+		for {
+
+			inline_objects := openapi3.SchemaRefs{}
+
+			for i := range def.AllOf {
+				compose_type := def.AllOf[i]
+
+				if compose_type.Ref == "" {
+					inline_objects = append(inline_objects, compose_type)
+					continue
+				}
+			}
+
+			if len(inline_objects) == 0 {
+				break
+			}
+
+			// Remove all inline objects in td.Schema.AllOf
+			for i := range inline_objects {
+				td.Schema.AllOf = RemoveSchemaRef(td.Schema.AllOf, inline_objects[i])
+			}
+
+			// Merge the inline objects into the main schema.
+			for i := range inline_objects {
+				c := inline_objects[i]
+				if td.Schema, err = MergeSchemaObjects(ctx, td.Schema, c.Value); err != nil {
+					return nil, err
+				}
+			}
+
 		}
 
-		component.Definition = &td
+		// Handle all references (all inline objects have been removed)
+		for i := range def.AllOf {
+			compose_type := def.AllOf[i]
+			// Reference to other type.
+			ref := ctx.resolver.ResolveComponent(componentId)
+			if ref == nil {
+				if ref, err = CreateComponentFromReference(ctx, componentId, compose_type); err != nil {
+					return nil, err
+				}
+			}
 
-	} else {
-		// This is a reference
-		component.Reference = ResolveReferenceAndSwitchIfNeeded(ctx, &component.ID, ref)
-		cached_type := ctx.resolver.ResolveComponent(component.Reference)
-
-		if cached_type != nil {
-			return component, nil
+			td.Composition = append(td.Composition, gentypes.Composition{
+				ComponentDefinition: *ref,
+				Inline:              false,
+			})
 		}
 
 	}
 
-	// TODO: If component contains definition -> Chase down properties, allOf, ...
-	// TODO: If component contains reference -> create type and put that into new component
-	// TODO: If the ref component.TypeName != component.ID.TypeName -> create a additional
-	// new type by shallow copy the ref component and change the type name.
+	// TODO: Chase down properties, allOf, ...
 
 	return component, nil
+}
+
+// RemoveSchemaRef will remove a _ref_ from the _references_ slice if found.
+func RemoveSchemaRef(references openapi3.SchemaRefs, ref *openapi3.SchemaRef) openapi3.SchemaRefs {
+	for j := range references {
+		if references[j] == ref {
+			return append(references[:j], references[j+1:]...)
+		}
+	}
+
+	return references
 }
 
 // ResolveReferenceAndSwitchIfNeeded will create a `ComponentReference`. If the _ref_ is under the specification root path
